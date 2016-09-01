72f61c520309ed2af2b41cc1b0e53a1126da04e2
aaa711 <liyating@baidu.com>
2016-09-01 11:04:35 +0800

    修改点击关注弹出提示toast三秒消失,并修改动态地址,一张图片点击放大

--- src/component/itemThree/threeItem.js
+++ src/component/itemThree/threeItem.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react'
 import {transferDate} from '../../util/util';
 import Gallery from '../gallery/gallery';
+import {clickHtml} from '../../util/util';
 
 require('./threeItem.less');
 
 class ThreeItem extends Component {
+    change=(id)=>{
+        let _hmt = _hmt || [];
+        clickHtml();
+        var val = $('#img'+id).attr('class');
+        var bool = val.indexOf('threeImg');
+        console.log(bool);
+        if(val.indexOf('threeImg')>=0){
+            console.log(id);
+            _hmt.push(['_trackEvent', '点击查看文章', 'click']);
+            $('#img'+id).removeClass('threeImg');
+            $('#img'+id).addClass('newPhoto');
+        }
+        else {
+            console.log('none');
+            $('#img'+id).removeClass('newPhoto');
+            $('#img'+id).addClass('threeImg');
+        }
+
+    }
+    urlFormate = (str)=>{
+        let exp = /src=(.+)$/;
+        let matchs = str.match(exp);
+        return !!matchs ? decodeURIComponent(matchs[1]) : '';
+    }
     render () {
         let itemData = this.props.itemData;
         let imageSrc = itemData.abstract.image;
         let len=imageSrc.length;
-        let galleryImgs=[];
+        let rows = [];
+        let urlFormate = [];
+        console.log(itemData);
         for(let i=0;i<len&&i<3;i++ ) {
-            let img = {src: imageSrc[i]};
-            galleryImgs.push(img);
+            urlFormate[i] = this.urlFormate(imageSrc[i]);
+            rows.push(
+                <div className="imgItem" key={itemData.id+'img'+i}>
+                    <img src = {urlFormate[i]} onClick={this.change.bind(this,this.props.itemData.id)} id={"img"+itemData.id}/>
+                </div>
+            );
         }
 
         return (
@@ -21,7 +52,7 @@ class ThreeItem extends Component {
                     <div className="text">
                         <h2>{itemData.abstract.text}</h2>
                         <div className="threeImg">
-                            <Gallery images={galleryImgs} userStyle="threeImg"></Gallery>
+                            {rows}
                         </div>
                         <div className="text-extra">
                             <div className="comment">{itemData.source}</div>
